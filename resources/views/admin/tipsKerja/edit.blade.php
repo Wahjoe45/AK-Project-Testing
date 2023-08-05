c31e17099ab823aa5d996ce4bc95a68663306e75
Wahjoe45 <setiawanaryawahjoe@gmail.com>
2023-08-05 16:42:16 +0700

    OTW edit tips kerja

--- resources/views/admin/tipsKerja/edit.blade.php
+++ resources/views/admin/tipsKerja/edit.blade.php
@@ -6,24 +6,31 @@
     <div class="content-header p-4">
         <div class="container-fluid ">
             <h3>Edit Post</h3>
+            
+            <!-- Script Judul Tips Kerja -->
             <div class="mb-3">
-                <input class="form-control" id="fullName" type="text" value="" placeholder="Masukan judul Tips Kerja....">
+                <input class="form-control" id="fullName" type="text" value="{{ old('judulTipsKerja') }}" placeholder="Masukan judul Tips Kerja....">
+                @error('judulTipsKerja')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
+
+            <!-- Script Konten Upload Gambar Tips Kerja -->
             <a href="">
-                <button type="button" class="btn btn-secondary pl-3 pr-3 mb-3"><i class="fa-solid fa-image mr-3"></i>Tambahkan Gambar</button>
+                <label for="fileInput" class="btn btn-secondary pl-3 pr-3">
+                <i class="fa-solid fa-image mr-3"></i>Tambahkan Gambar
+                <input type="file" name="gambarTipsKerja" id="fileInput" accept="image/*" class="d-none">
+                </label>
             </a>
-            <div class="mb-3"> 
-                    <textarea class="form-control" rows="3" id="ck-editor"></textarea>
-                    <!-- Script Text Area CK-Editor -->
-                    <script>
-                        ClassicEditor
-                        .create( document.querySelector( '#ck-editor' ) )
-                        .catch( error => {
-                            console.error( error );
-                        } );
-                    </script>
-                    <!-- Script Text Area CK-Editor -->
+
+            <!-- Script Konten Tips Kerja -->
+            <div class="mb-3">
+                <input class="form-control" id="fullName" type="text" value="{{ old('contentTipsKerja') }}" placeholder="Masukan konten tips kerja....">
+                @error('contentTipsKerja')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="col-12 text-right">
                 <a href="adminTipsKerja">
                     <button type="button" class="btn btn-success pl-4 pr-4">Simpan</button>
